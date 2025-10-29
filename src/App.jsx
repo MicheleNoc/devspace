7f00e17fc5a0afd6fe7e9a94783debbf17e76ba0
Michele <nocchimichele13@gmail.com>
2025-10-29 17:55:44 +0100

    Aggiornamento delle dipendenze Supabase e refactoring dell'app per utilizzare l'autenticazione di Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+  import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+  import { Auth } from '@supabase/auth-ui-react';
+  import { ThemeSupa } from '@supabase/auth-ui-shared';
+  import supabase from './supabaseClient';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-3xl font-bold underline">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  export default function App() {
+    const [session, setSession] = useState(null);
 
-export default App
+    useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+      })
+
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+      })
+
+      return () => subscription.unsubscribe()
+    }, [])
+
+    if (!session) {
+      return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+    }
+    else {
+      return (<div className='text-green-500'>Logged in!</div>)
+    }
+  }
